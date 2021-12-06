5b48f7ab197ba3b32bf2a7c18045dd34654008b4
Lucian Dinu <luciandesign@gmail.com>
2021-12-06 10:46:22 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import SiteFellows from "./sitefellows";
 import SiteFellowsUI from "./sfui";
-// import "./sitefellows.css";
-
-// global.SFC = SFC;
-// global.Utils = Utils;
-//global.SiteFellows = SiteFellows;
-//window['SiteFellowsUI']= SiteFellowsUI;
-
 
 SiteFellows._Init();
 SiteFellowsUI._Init();
